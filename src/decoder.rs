5870950278f9990ad79855b5af39d00d23a040df
Andy Ealovega <andrew@ealovega.dev>
2021-08-16 00:19:14 -0400

    Add tests and doc.

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,6 +1,7 @@
 use crate::error::Sec1PemError;
 use der::asn1::{ContextSpecific, OctetString};
 pub use der::{Decodable, Message};
+use std::option::Option::Some;
 
 #[derive(Clone, Debug, Eq, PartialEq, Message)]
 /// Direct intermediate representation of the der sequence. This is just a view over that sequence,
@@ -18,7 +19,7 @@ pub struct EcPrivateKey {
     pub version: u8,
     /// The private key bytes.
     pub key: Vec<u8>,
-    pub curve: Option<Vec<u8>>,//TODO reasurch what these really are, then test.
+    pub curve: Option<Vec<u8>>, //TODO change this to a curve tag type
     /// The private key bytes, if included.
     pub public_key: Option<Vec<u8>>,
 }
@@ -42,24 +43,28 @@ impl From<EcPrivateKey_<'_>> for EcPrivateKey {
 }
 
 const PEM_EC_HEADER: &str = "EC PRIVATE KEY";
-const PEM_EC_HEADER_PUB: &str = "EC PUBLIC KEY";
 
+/// Attempts to parse an EcPrivateKey sequence from the passed bytes.
+/// ```
+/// # use sec1_pem::parse_pem;
+/// const PEM:&str = "-----BEGIN EC PRIVATE KEY-----
+/// MHcCAQEEIASgox4rXoGc6ajVAjBCsjVIjbfHd8OK3m5v34ZWVBmmoAoGCCqGSM49
+/// AwEHoUQDQgAEUfXAsSR5LH4rVdHbcK1vnYcN9I/6T7u1bl1RprSZFf89aZXL+CeG
+/// G21XVW8IDhjU7HAXgrO1Sqj00zQtluVBTg==
+/// -----END EC PRIVATE KEY-----";
+///
+/// let parsed = parse_pem(PEM.as_bytes()).unwrap();
+/// ```
 pub fn parse_pem(bytes: &[u8]) -> Result<EcPrivateKey, Sec1PemError> {
+    //Parse many to account for curve params pem
     let parsed = pem::parse_many(bytes);
 
-    //Merge bytes of all relevant sequences. This is done to ignore things like curve params.
-    let pk_pem: Vec<u8> = parsed
-        .into_iter()
-        .filter(|pem| pem.tag == PEM_EC_HEADER || pem.tag == PEM_EC_HEADER_PUB) //Include both private and public key sequences
-        .fold(Vec::new(), |mut acc, item| {
-            acc.push(item.contents);
-            acc
-        })
-        .concat();
+    let pk_pem = parsed.into_iter().find(|pem| pem.tag == PEM_EC_HEADER);
 
-    if !pk_pem.is_empty() {
+    if let Some(pk_pem) = pk_pem {
         //Parse der sequence into struct
-        let key = EcPrivateKey_::from_der(&pk_pem).unwrap();//TODO handle
+        let key =
+            EcPrivateKey_::from_der(&pk_pem.contents).map_err(|_| Sec1PemError::ParseError)?;
         Ok(key.into())
     } else {
         Err(Sec1PemError::NoECPrivateKey)
@@ -68,12 +73,70 @@ pub fn parse_pem(bytes: &[u8]) -> Result<EcPrivateKey, Sec1PemError> {
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use der::Decodable;
+    use std::path;
+    use std::path::PathBuf;
 
-    const PEM: &str = "MHcCAQEEIMwug/U2ds75hkEIeou9s0kj1ziCJETswt5S9ztJ2L5SoAoGCCqGSM49AwEHoUQDQgAEyjUeooXqyQxljKSu17126pjAEPTyYNApO6dGQl0PexMn0T7LI3qwmU9ZOko2Gn7LYp5LqgA0cX6rfDftsKVvtQ==";
+    fn get_path() -> PathBuf {
+        let mut path = path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("resources/");
+        path
+    }
 
     #[test]
-    fn test_parse() { //TODO convert this to usable code and then test more with actual certs.
+    /// Tests that more than one PEM will parse. Does not verify the keys are correct.
+    fn test_pems() {
+        let path = get_path();
+
+        //Test each pem
+        for file in path.read_dir().unwrap() {
+            let path = file.unwrap().path();
+
+            //Skip the bad key
+            if path.ends_with("pkcs8eckey.pem") {
+                continue;
+            }
+
+            let pem = std::fs::read_to_string(path).unwrap();
+
+            let parse = crate::parse_pem(pem.as_bytes()).unwrap();
+
+            println!("Parsed: {:?}", parse);
+
+            assert_eq!(parse.version, 1);
+            assert!(!parse.key.is_empty());
+            assert_eq!(parse.key.len(), 32); //32 * 8 bytes = 256 byte key (we're using P256)
+            assert!(parse.public_key.is_some());
+            assert!(parse.curve.is_some());
+        }
+    }
+
+    #[test]
+    /// Tests that a known key parses.
+    fn test_key_parses() {
+        let mut path = get_path();
+        path.push("private_key2.pem"); //This is the key without a curve header.
+        let pem = std::fs::read_to_string(path).unwrap();
+        let expected =
+            hex::decode("04A0A31E2B5E819CE9A8D5023042B235488DB7C777C38ADE6E6FDF86565419A6") //The key in hex form, dumped from openssl
+                .unwrap();
+
+        let parsed = crate::parse_pem(pem.as_bytes()).unwrap();
+
+        assert_eq!(expected, parsed.key)
+    }
+
+    #[test]
+    /// Tests that non ECPrivateKeys are not accepted.
+    fn test_pkcs8_fails() {
+        let mut path = get_path();
+        path.push("pkcs8eckey.pem"); //pkcs8 encrypted key.
+        let pem = std::fs::read_to_string(path).unwrap();
+
+        let fails = crate::parse_pem(pem.as_bytes());
+
+        assert_eq!(
+            fails.unwrap_err(),
+            crate::error::Sec1PemError::NoECPrivateKey
+        )
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,11 @@
+use thiserror::Error;
 
+#[derive(Error, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
 pub enum Sec1PemError {
     /// No sequence headed by "EC PRIVATE KEY" in parsed bytes.
-    NoECPrivateKey
+    #[error("there was no EcPrivateKey sequence in this file.")]
+    NoECPrivateKey,
+    /// There was an issue while parsing the key.
+    #[error("There was an error while parsing the sequence.")]
+    ParseError,
 }
